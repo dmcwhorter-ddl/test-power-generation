4d133b85df48f81e59d15b78064cbb2909098626
David McWhorter <david.mcwhorter@dominodatalab.com>
2022-06-16 15:41:00 -0400

    Pushed from Domino

--- forecast_launcher.sh
+++ forecast_launcher.sh
@@ -2,7 +2,8 @@ python -m venv --system-site-packages .
 
 source ./bin/activate
 
-pip install "pystan==2.19.1.1" "plotly<4.0.0" "papermill<2.0.0" requests dash
+pip install "cython=0.29.30"
+pip install "pystan==2.19.1.1" "plotly" "papermill" requests dash
 pip install prophet
 
 papermill Forecast_Power_Generation_Launcher.ipynb forecast.ipynb -p start_date "$1" -p fuel_type $2
