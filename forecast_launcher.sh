5cc1680bf94967bb54abbe94c0c87a7fcc22c670
David McWhorter <david.mcwhorter@dominodatalab.com>
2022-06-16 15:39:49 -0400

    don't bound papermill

--- forecast_launcher.sh
+++ forecast_launcher.sh
@@ -1,5 +1,5 @@
 python -m venv --system-site-packages .
 
-./bin/pip install 'papermill<2.0.0'
+./bin/pip install 'papermill'
 
 ./bin/papermill Forecast_Power_Generation_Launcher.ipynb forecast.ipynb -p start_date "$1" -p fuel_type $2
