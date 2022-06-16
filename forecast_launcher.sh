b9da2f01b00c651b51328d8c6803d5d944fc7ea6
David McWhorter <david.mcwhorter@dominodatalab.com>
2022-06-16 15:39:31 -0400

    use venv for papermill

--- forecast_launcher.sh
+++ forecast_launcher.sh
@@ -1,1 +1,5 @@
-papermill Forecast_Power_Generation_App.ipynb forecast.ipynb -p start_date "$1" -p fuel_type $2
+python -m venv --system-site-packages .
+
+./bin/pip install papermill<2.0.0
+
+./bin/papermill Forecast_Power_Generation_App.ipynb forecast.ipynb -p start_date "$1" -p fuel_type $2
