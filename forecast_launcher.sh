c1470fc21a89365cf4d200d0277c15e38cd79ade
David McWhorter <david.mcwhorter@dominodatalab.com>
2022-06-16 15:40:20 -0400

    remove <2.0.0

--- forecast_launcher.sh
+++ forecast_launcher.sh
@@ -2,6 +2,6 @@ python -m venv --system-site-packages .
 
 source ./bin/activate
 
-pip install 'papermill<2.0.0'
+pip install 'papermill'
 
 papermill Forecast_Power_Generation_Launcher.ipynb forecast.ipynb -p start_date "$1" -p fuel_type $2
